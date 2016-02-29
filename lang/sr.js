f42900938a799f8aaab4fcfc8978514664fb6a3a
Ivan Jovanovic <ij.smartnet@gmail.com>
2016-02-29 17:08:06 +0100

    CMS-35 updated sr.js file

---
+++ lang/sr.js
@@ -0,0 +1,16 @@
+!function() {
+    Polymer.atI18nLocales.add('global', 'sr-SR', {
+        'The capital of {0} is {1}.': 'Glavni grad {0} je {1}.',
+
+        // at-form-*
+       'This field is required.': 'Ovo polje je obavezno.'
+    });
+}();
+
+
+!function() {
+    Polymer.atI18nLocales.add('demo-i18n', 'sr-SR', {
+        hello: 'Zdravo',
+        'My name is {0} and I am from {1}.': 'Ja se zovem {0} i iz {1} sam.'
+    });
+}();
