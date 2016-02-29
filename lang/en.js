91257ff4fe70f3002a92744a5ffade0cea05ee57
Martin Amm <martin.amm@adenin.com>
2016-02-29 10:04:56 -0500

    initial prototype

---
+++ lang/en.js
@@ -0,0 +1,13 @@
+!function() {
+    Polymer.atI18nLocales.add('global', 'en', {               
+        
+        // at-form-*
+       'This field is required.': 'This field is required.'      
+    });
+}();
+
+!function() {
+    Polymer.atI18nLocales.add('demo-i18n', 'en', {
+        hello: 'Hello World'       
+    });
+}();
