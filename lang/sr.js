409979a007aeab77c7d75decb25bdb11d34c7f4f
Ivan Jovanovic <ij.smartnet@gmail.com>
2016-03-01 09:38:48 +0100

    CMS-35 updated sr.js file

--- lang/sr.js
+++ lang/sr.js
@@ -1,5 +1,5 @@
 !function() {
-    Polymer.atI18nLocales.add('global', 'sr-SR', {
+    Polymer.atI18nLocales.add('global', 'sr', {
         'The capital of {0} is {1}.': 'Glavni grad {0} je {1}.',
 
         // at-form-*
@@ -9,7 +9,7 @@
 
 
 !function() {
-    Polymer.atI18nLocales.add('demo-i18n', 'sr-SR', {
+    Polymer.atI18nLocales.add('demo-i18n', 'sr', {
         hello: 'Zdravo',
         'My name is {0} and I am from {1}.': 'Ja se zovem {0} i iz {1} sam.'
     });
