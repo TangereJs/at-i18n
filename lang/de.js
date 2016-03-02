ab7ce14475cc69d3da2db0132df14861980d4992
Martin Amm <martin.amm@adenin.com>
2016-03-02 08:35:12 -0500

    reformatted, added 'Input required'

--- lang/de.js
+++ lang/de.js
@@ -1,24 +1,25 @@
-!function() {
-    Polymer.atI18nLocales.add('global', 'de', {
-        'The capital of {0} is {1}.': '{1} ist die Hauptstadt von {0}.',
+!function () {
+  Polymer.atI18nLocales.add('global', 'de', {
+    'The capital of {0} is {1}.': '{1} ist die Hauptstadt von {0}.',
 
-        // format definitions
-        '$format-date': 'DD.MM.YYYY',
-        '$format-time': 'HH:mm:ss',
-        '$format-datetime': 'DD.MM.YYYY HH:mm:ss',
+    // format definitions
+    '$format-date': 'DD.MM.YYYY',
+    '$format-time': 'HH:mm:ss',
+    '$format-datetime': 'DD.MM.YYYY HH:mm:ss',
 
-        'Submit': 'Einreichen',
-        'Cancel': 'Stornieren',
+    'Submit': 'Speichern',
+    'Cancel': 'Abbrechen',
 
-        // at-form-*
-       'This field is required.': 'In diesem Feld ist eine Eingabe erforderlich.'
-    });
+    // at-form-*
+    'Input required': 'Eingabe erforderlich',
+    'This field is required.': 'In diesem Feld ist eine Eingabe erforderlich.'
+  });
 }();
 
 
-!function() {
-    Polymer.atI18nLocales.add('demo-i18n', 'de', {
-        hello: 'Hallo Welt',
-        'My name is {0} and I am from {1}.': 'Mein Name ist {0} und ich bin aus {1}.'
-    });
+!function () {
+  Polymer.atI18nLocales.add('demo-i18n', 'de', {
+    hello: 'Hallo Welt',
+    'My name is {0} and I am from {1}.': 'Mein Name ist {0} und ich bin aus {1}.'
+  });
 }();
--- lang/en.js
+++ lang/en.js
@@ -1,21 +1,23 @@
-!function() {
-    Polymer.atI18nLocales.add('global', 'en', {
+!function () {
+  Polymer.atI18nLocales.add('global', 'en', {
 
-        // format definitions
-        '$format-date': 'M/D/YYYY',
-        '$format-time': 'HH:mm:ss',
-        '$format-datetime': 'D/M/YYYY HH:mm:ss',
+    // format definitions
+    '$format-date': 'M/D/YYYY',
+    '$format-time': 'HH:mm:ss',
+    '$format-datetime': 'D/M/YYYY HH:mm:ss',
 
-        'Submit': 'Submit',
-        'Cancel': 'Cancel',
+    'Submit': 'Submit',
+    'Cancel': 'Cancel',
 
-        // at-form-*
-       'This field is required.': 'This field is required.'
-    });
+    // at-form-*
+    'Input required': 'Input required',
+    'This field is required.': 'This field is required.'
+  });
 }();
 
-!function() {
-    Polymer.atI18nLocales.add('demo-i18n', 'en', {
-        hello: 'Hello World'
-    });
+
+!function () {
+  Polymer.atI18nLocales.add('demo-i18n', 'en', {
+    hello: 'Hello World'
+  });
 }();
